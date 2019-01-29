29cf90f4991c04323b5793cf30a5614fb7adf544
Wallef Antoine <antoine.wallef@gmail.com>
2019-01-29 10:33:01 +0100

    vue fix

--- webroot/js/front/app/app.js
+++ webroot/js/front/app/app.js
@@ -4,13 +4,6 @@
   *************************/
   var initViewJs = function()
   {
-    Vue.http.interceptors.unshift(function(request, next) {
-      next(function(response) {
-        if(typeof response.headers['content-type'] != 'undefined') {
-          response.headers['Content-Type'] = response.headers['content-type'];
-        }
-      });
-    });
     scope.eventHub = new Vue();
     scope.frontApp = new Vue({el: "#front-app"});
   }
