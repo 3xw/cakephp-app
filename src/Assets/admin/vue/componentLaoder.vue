ca3fe894b203b87c85d3b2c2e917989bd5e28ec2
jeanvier <damien.grossfeld@gmail.com>
2019-11-13 09:47:02 +0100

    Component loader update

--- src/Assets/admin/vue/componentLaoder.vue
+++ src/Assets/admin/vue/componentLaoder.vue
@@ -7,18 +7,40 @@ export default {
     name: { type: String, default: 'null' },
     props: { type: String }
   },
-  data: function(){
-    return {
-      attributes: JSON.parse(this.props)
+  computed:
+  {
+    componentInstance ()
+    {
+      if (this.name == 'null') {
+        return null
+      }
+      const name = this.camelize(this.name.substring(this.name.indexOf('-') + 1))
+      return () => import(/* webpackChunkName: "[request]" */ `./${name}.vue`)
+    },
+    attributes()
+    {
+      let baseObj = JSON.parse(this.props);
+      let obj = {}
+      for(let i in baseObj)
+      {
+        if(i.substr(0,1) == ':')
+        {
+          let prop = i.substr(1)
+          obj[prop] = baseObj[i]
+        }
+        else obj[i] = baseObj[i]
+      }
+      console.log(obj);
+      return obj
     }
   },
-  computed: {
-    componentInstance () {
-      if (this.name == 'null') {
-         return null
-      }
-      const name = this.name
-      return () => import(/* webpackChunkName: "[request]" */ `./${name}.vue`)
+  methods:
+  {
+    camelize(str) {
+      return str.replace(/(?:^\w|[A-Z]|-|\b\w)/g, function(word, index)
+      {
+        return word.toUpperCase();
+      }).replace(/\s+/g, '');
     }
   }
 }
