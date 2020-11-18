6a20d9b2846bfff65d4111427a456f9ae54024c4
awallef <antoine.wallef@gmail.com>
2020-11-18 08:45:58 +0100

    cookie delete ok

--- templates/Error/error_cookie_consent.php
+++ templates/Error/error_cookie_consent.php
@@ -3,9 +3,15 @@ use Cake\Core\Configure;
 use Trois\Utils\Http\Middleware\CookieConsentMiddleware as Cookie;
 if(Cookie::$exists && !Cookie::$allow)
 {
-  /* not working */
+  // php vars
   $cookieName = Configure::read('CookieConsent.cookieName');
-  $script = "document.cookie = '$cookieName' + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';";
+  $host = $_SERVER['HTTP_HOST'];
+
+  // js
+  $script = "window.addEventListener('load', function(){\n";
+  $script .= "\tdocument.cookie = \"$cookieName=;expires= Thu, 01 Jan 1970 00:00:00 GMT;path=/\"\n";
+  $script .= "\tconsole.log('delete cookie')\n";
+  $script .= "})\n";
   $this->Html->scriptBlock($script, ['block' => true]);
 }
 ?>
@@ -15,10 +21,10 @@ if(Cookie::$exists && !Cookie::$allow)
   <div class="jumbotron">
   <h1 class="display-4">We are sorry, but you must allow our cookies to use this functionality</h1>
   <h1 class="display-4">🍪</h1>
-  <p class="lead">First allow cokies, and then click on the button below.</p>
+  <p class="lead">Please follow the link bellow and consent to cookies.</p>
   <hr class="my-4">
   <p class="lead">
-    <?= $this->Html->link('Go back', '/', ['class' => 'btn btn-primary btn-lg']) ?>
+    <?= $this->Html->link('Go back to intro', '/', ['class' => 'btn btn-primary btn-lg']) ?>
   </p>
 </div>
 </div>
